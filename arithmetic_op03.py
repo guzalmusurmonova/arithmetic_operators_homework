ee7c6652061f131c25f03d964060743c011b846d
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-07 06:27:22 -0700

    03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,7 +3,7 @@ number=57
 #Find the reverse of the number and assign it to a variable called 'answer'.
 a=number%10
 b=number//10
-javob=10*a+b
+answer=10*a+b
 #Print the value of the 'answer'
-print(javob)
+print(answer)
 
